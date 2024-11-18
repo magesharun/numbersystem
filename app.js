6baf28ca802f081eee77cba92a87a831e94db71b
magesharun <magesharun2016@gmail.com>
2024-11-18 18:28:25 +0530

    final commit

---
+++ app.js
@@ -0,0 +1,37 @@
+// const primeArray = [
+//     19, 23, 4, 16, 28, 13, 31, 8, 29, 14, 6, 35, 2, 11, 17, 5, 9, 27, 12, 30,
+//   ];
+  
+//   let max = primeArray[0]; 
+//   let min = primeArray[0]; 
+
+  
+//   for (let i = 1; i < primeArray.length; i++) {
+//     if (primeArray[i] > max) {
+//       max = primeArray[i];
+//     }
+//     if (primeArray[i] < min) {
+//       min = primeArray[i];
+//     }
+//   }
+  
+//   console.log("Maximum value:", max);
+//   console.log("Minimum value:", min);
+
+
+//   const primeArray=[
+//     19, 23, 4, 16, 28, 13, 31, 8, 29, 14, 6, 35, 2, 11, 17, 5, 9, 27, 12, 30,
+//   ];
+
+//   for(let i=0;i<primeArray.length;i++) {
+
+//     if(primeArray[i]!=0)
+//     {
+//         sum=sum+primeArray[i];
+//         console.log("sum of prime numbers in array"+sum);
+        
+//     }
+
+//   }
+  
+  
